f11d19c2a9efea42f6f1b770091574f7d172319a
KavindaMethsara <methsara31@gmail.com>
2023-08-10 00:45:41 +0530

    Updated the landing page and working on mobile.r

--- src/app/components/Section02.tsx
+++ src/app/components/Section02.tsx
@@ -2,44 +2,72 @@ import React from 'react';
 
 const Section02 = () => {
   return (
-    <div className="min-h-screen flex flex-col p-12 bg-white">
+    <div className="min-h-screen flex flex-col p-12 bg-slate-100">
       {/* Add the content for the section01 here */}
       <div className="text-center items-center justify-center mb-16">
-        <h2 className="text-5xl text-black font-semibold mb-4">Campus News</h2>
+        <h2 className="text-5xl text-black font-bold mb-4">Campus News</h2>
       </div>
       <div className="text-center mb-28 items-center justify-center">
-        <p className="text-4xl text-black font-thin">
+        <p className="lg:text-4xl text-black font-thin">
           Stories about people, research, and innovation across the Farm
         </p>
       </div>
-      <div className="grid lg:grid-cols-3 gap-8">
+      <div className="grid sm:grid-rows-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {/* Card 1 */}
-        <div className="bg-white shadow-lg p-2">
-          <img src="./cmps news-card01.jpg" alt="" className='w-full h-full' />
+        <div className="col-span-2 bg-white shadow-lg p-2">
+        <img src="./cmps news-card01.jpg" alt="" className='w-full h-96' />
         </div>
         {/* Card 2 */}
-        <div className="bg-white shadow-lg p-2">
-        <img src="./cmps news-card02.jpg" alt="" className='w-full h-full' />
-          {/* Content for Card 2 */}
+        <div className="max-w-sm bg-white p-2">
+          <a href="#">
+              <img src="./cmps news-card02.jpg" alt="" className="w-full h-auto"/>
+          </a>
+          <div className="p-5">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-red-900">SCIENCE & TECHNOLOGY</h5>
+            <a href="#">
+              <p className="mb-3 font-bold text-black hover:text-blue-700">Mosquito diseases on the move</p>
+            </a>
+          </div>
         </div>
         {/* Card 3 */}
-        <div className="bg-white shadow-lg p-2">
-        <img src="./cmps news-card03.jpg" alt="" className='w-full h-full' />
-          {/* Content for Card 3 */}
+        <div className="max-w-sm bg-white p-2">
+          <a href="#">
+              <img src="./cmps news-card03.jpg" alt="" className="w-full h-auto"/>
+          </a>
+          <div className="p-5">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-red-900">SOCIAL SCIENCES</h5>
+            <a href="#">
+              <p className="mb-3 font-bold text-black hover:text-blue-700">How a CEO’s personality affects corporate culture</p>
+            </a>
+          </div>
         </div>
         {/* Card 4 */}
-        <div className="bg-white shadow-lg p-2">
-        <img src="./cmps news-card04.jpg" alt="" className='w-full h-full' />
+        <div className="max-w-sm bg-white p-2">
+          <a href="#">
+              <img src="./cmps news-card04.jpg" alt="" className="w-full h-auto"/>
+          </a>
+          <div className="p-5">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-red-900">MEDICINE</h5>
+            <a href="#">
+              <p className="mb-3 font-bold text-black hover:text-blue-700">Inclusive data is a boon to LGBTQ health researchers</p>
+            </a>
+          </div>
         </div>
         {/* Card 5 */}
-        <div className="bg-white shadow-lg p-2">
-        <img src="./cmps news-card05.jpg" alt="" className='w-full h-full' />
-          {/* Content for Card 5 */}
+        <div className="max-w-sm bg-white p-2">
+          <a href="#">
+              <img src="./cmps news-card05.jpg" alt="" className="w-full h-auto"/>
+          </a>
+          <div className="p-5">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-red-900">UNIVERSITY AFFAIRS</h5>
+            <a href="#">
+              <p className="mb-3 font-bold text-black hover:text-blue-700">Stanford Board of Trustees release of report and announcements</p>
+            </a>
+          </div>
         </div>
         {/* Card 6 */}
-        <div className="bg-white shadow-lg p-2">
-        <img src="./cmps news-card06.jpg" alt="" className='w-full h-full' />
-          {/* Content for Card 6 */}
+        <div className="col-span-2 bg-white shadow-lg p-2">
+        <img src="./cmps news-card06.jpg" alt="" className='w-full h-96' />
         </div>
       </div>
       <div className='text-center items-center justify-center'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import Navbar from '../app/components/Navbar'; // Check the relative path
 import Home from '../app/components/Home'; // Check the relative path
 import Section01 from '../app/components/Section01'; // Check the relative path
 import Section02 from '../app/components/Section02'; // Check the relative path
-import Section03 from '../app/components/Section03'; // Check the relative path
-import Section04 from '../app/components/Section04'; // Check the relative path
-import Section05 from '../app/components/Section05'; // Check the relative path
-import Section06 from '../app/components/Section06'; // Check the relative path
-import Section07 from '../app/components/Section07'; // Check the relative path
-import Section08 from '../app/components/Section08'; // Check the relative path
+// import Section03 from '../app/components/Section03'; // Check the relative path
+// import Section04 from '../app/components/Section04'; // Check the relative path
+// import Section05 from '../app/components/Section05'; // Check the relative path
+// import Section06 from '../app/components/Section06'; // Check the relative path
+// import Section07 from '../app/components/Section07'; // Check the relative path
+// import Section08 from '../app/components/Section08'; // Check the relative path
 import Section09 from '../app/components/Section09'; // Check the relative path
 import Section10 from './components/Section10';
 import Section11 from './components/Section11';
@@ -20,12 +20,12 @@ const Page = () => {
       <Navbar />
       <Section01 />
       <Section02 />
-      <Section03 />
+      {/* <Section03 />
       <Section04 />
       <Section05 />
       <Section06 />
       <Section07 />
-      <Section08 />
+      <Section08 /> */}
       <Section09 />
       <Section10 />
       <Section11 />
